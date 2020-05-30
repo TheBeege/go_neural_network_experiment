203596d88ddc7b064e279a963099e1a554559538
Bryan "Beege" Berry <bryan.berry@posicube.com>
2020-05-31 02:20:17 +0900

    feat: Setup cmd via cobra

---
+++ main.go
@@ -0,0 +1,10 @@
+// Copyright © 2020 Posicube <contact@posicube.com>
+// Posicube Proprietary
+
+package main
+
+import "github.com/TheBeege/go_neural_network_experiment/cmd"
+
+func main() {
+	cmd.Execute()
+}
